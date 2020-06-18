8591ec83a8215096c6880119d2549abe63bc6f37
christianangel15 <christianangel15@gmail.com>
2020-06-18 15:46:50 -0500

    Minor edits

--- covid19.py
+++ covid19.py
@@ -9,13 +9,14 @@ import json
 # print(data['Global'])
 # with open('Covid19.json', 'w') as f:
 #     json.dump(data, f, indent=2)
-with open('Covid19.json', 'r') as f:
+with open('Pandas_demo/Covid19JHU.json', 'r') as f:
     data = json.load(f)
 
-for country in data['Countries']:
-    Active = country['TotalConfirmed'] - \
-        country['TotalDeaths'] - country['TotalRecovered']
-    if (country['Country'] == 'India'):
-        print(country['Country'])
-        print(f'Active Cases - {Active}')
-        print()
+print(data['Active_Cases']['US'])
+# for country in data['Countries']:
+#     Active = country['TotalConfirmed'] - \
+#         country['TotalDeaths'] - country['TotalRecovered']
+#     if (country['Country'] == 'United States of America'):
+#         print(country['Country'])
+#         print(f'Active Cases - {Active}')
+#         print()

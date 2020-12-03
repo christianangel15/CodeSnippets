cf46b5d6efbdd0635059783196f8933c7bbc8f15
christianangel15 <christianangel15@gmail.com>
2020-12-03 12:57:09 -0600

    importtogbq updated

--- importtogbq.py
+++ importtogbq.py
@@ -6,11 +6,10 @@ from google.oauth2 import service_account
 credentials = service_account.Credentials.from_service_account_file(
     os.environ.get('GOOGLE_APPLICATION_CREDENTIALS'))
 # From CSV - local directory
-df = pd.read_csv('COVID Data Pulling\data\ETH_1hr.csv')
+df = pd.read_csv('data.csv')
 # From JSON - local directory
 # df = pd.read_json('COVID Data Pulling\data\sample.json')
 
-
-df.to_gbq(destination_table='Test.SampleTable',
-          project_id='testbq-296623', if_exists='fail', credentials=credentials)
+df.to_gbq(destination_table='Test.Angel',
+          project_id='testbq-296623', if_exists='append', credentials=credentials)
 print('Data Uploaded')

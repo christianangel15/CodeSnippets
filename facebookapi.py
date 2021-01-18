84275825f3b85196b9122f87cc6c165b68eb2a9d
Angel Christian <christianangel15@gmail.com>
2021-01-18 09:19:42 -0600

    Delete facebookapi.py

--- facebookapi.py
+++
@@ -1,9 +0,0 @@
-import facebook
-token = 'Your access token here'
-fbobj = facebook.GraphAPI(access_token=token)
-# try:
-# fbobj.put_object('me', 'feed', message='Posted using Graph API')
-fbobj.put_photo(image=open('photo-1522364723953-452d3431c267.jpg', 'rb'),
-                message='Photo posted using Graph API..Cool!')
-# ca:
-print('Posted')
